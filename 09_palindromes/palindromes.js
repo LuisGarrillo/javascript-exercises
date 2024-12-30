ad60cac0d3d691f88dd14a8670f90c2c6271d796
Mel <garrillofeeling@gmail.com>
2024-12-30 18:00:37 -0400

    Add solution to exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toLowerCase();
+    let reversedWord = "";
+    let normalWord = "";
+    for (let i = word.length - 1; i >= 0; i--) {
+        if ((word[i].charCodeAt(0) >= 97 && word[i].charCodeAt(0) <= 122) || (word[i].charCodeAt(0) >= 48 && word[i].charCodeAt(0) <= 57))
+            reversedWord += word[i];
+    }
+    for (let i = reversedWord.length - 1; i >= 0; i--) {
+        normalWord +=  reversedWord[i]
+    }
+    return normalWord === reversedWord;
 };
 
 // Do not edit below this line
