8c4c4e733aff863fde67334ee5df32f154f29f64
Mel <garrillofeeling@gmail.com>
2024-12-31 15:06:35 -0400

    Add solution to exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce(
+        (oldest, person) => {
+            if (!oldest)
+                return person;
+            const year = new Date().getFullYear();
+            const personAge = ((person.yearOfDeath) ? person.yearOfDeath : year) - person.yearOfBirth;
+            const oldestAge = ((oldest.yearOfDeath) ? oldest.yearOfDeath : year) - oldest.yearOfBirth;
+            return (personAge > oldestAge) ? person : oldest;
+        }
+    );
 };
 
 // Do not edit below this line
