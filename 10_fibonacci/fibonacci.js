aca552bba3da4835e6ab47a90c5cd0e02d37ce9f
Mel <garrillofeeling@gmail.com>
2024-12-31 13:09:15 -0400

    Add solution to exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(member, memberCount = member, sequence = [1]) {
+    if (member == 0)
+        return 0
+    else if (member < 0)
+        return "OOPS";
+    
+    if (memberCount == 1)
+        return sequence[member - 1];
 
+    let nextmember;
+    let workingSequence = (sequence.length > 2) ? sequence.slice(sequence.length - 2) : sequence;
+    
+    nextmember = workingSequence.reduce((total, current) => total + current, 0);
+    sequence.push(nextmember);
+    return fibonacci(member, memberCount - 1, sequence);
 };
 
 // Do not edit below this line
